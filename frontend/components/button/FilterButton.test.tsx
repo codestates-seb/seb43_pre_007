e27fc2e9b69a9478ed0961146c8311e29b4e1a7b
dev-Jaewon <app235naver@gmail.com>
2023-04-15 10:03:51 +0900

    refact - filter 버튼 firevent에서 userEvent로 전환

--- frontend/components/button/FilterButton.test.tsx
+++ frontend/components/button/FilterButton.test.tsx
@@ -1,8 +1,10 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { FilterButton } from './FilterButton';
+import userEvent from '@testing-library/user-event';
 
 describe('<FilterButton />', () => {
   const handeChnage = jest.fn();
+  const user = userEvent.setup();
 
   beforeEach(() => {
     render(
@@ -14,24 +16,30 @@ describe('<FilterButton />', () => {
     );
   });
 
-  test('render', () => {
+  test('렌더링', () => {
     const { getByRole } = screen;
     expect(getByRole('list')).toBeInTheDocument();
   });
 
-  test('render children', () => {
+  test('filters props에 입력 데이터가 자식 Element로 렌더링 된다.', () => {
     const { getByRole } = screen;
     expect(getByRole('list').childElementCount).toBe(2);
   });
 
-  test('set default props', () => {
+  test('default props로 데이터를 입력하면 기본 값으로 선택된다.', () => {
     const { getByText } = screen;
     expect(getByText(/흥미/)).toHaveStyle('background-color: rgb(228,229,231)');
   });
 
-  test('click filter', () => {
+  test('filter 리스트 중 하나를 클릭하면 onChange로 callback 함수가 실행된다.', async () => {
     const { getByText } = screen;
-    fireEvent.click(getByText(/인기/));
+    await user.click(getByText(/인기/));
     expect(handeChnage).toBeCalledWith('인기');
+  });
+
+  test('filter 리스트 중 하나에 마우스를 올렸을 경우 백그라운드 색이 변한다.', async () => {
+    const { getByText } = screen;
+    await user.hover(getByText(/흥미/));
+    expect(getByText(/흥미/)).toHaveStyle('background-color: rgb(228,229,231)');
   });
 });
