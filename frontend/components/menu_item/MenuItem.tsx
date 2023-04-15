41affc5cad1ddf07a29ffb7be0ca81b3cb592b0a
shdomi8599 <shdomi@naver.com>
2023-04-16 00:28:30 +0900

    style : 기본 스타일 지정

--- frontend/components/menu_item/MenuItem.tsx
+++ frontend/components/menu_item/MenuItem.tsx
@@ -13,6 +13,22 @@ export default MenuItem;
 
 const MenuItemContainer = styled.a<MenuItemProps>`
   width: 100%;
+  font-size: 0.8rem;
+  color: #867f78;
+  padding: calc(6px * 1) calc(12px * 1);
+  align-items: center;
+  border-radius: 1000px;
+  display: flex;
+  position: relative;
+  vertical-align: middle;
+  cursor: pointer;
+  :hover {
+    color: black;
+    background-color: #ececec;
+  }
+  @media (max-width: 740px) {
+    font-size: 0.7rem;
+  }
   color: ${(props) => (props.idx === props.pick ? 'white' : '')};
   background-color: ${(props) =>
     props.idx === props.pick ? 'var(--bg-orange)' : ''};
--- frontend/pages/users/[userId]/[userName].tsx
+++ frontend/pages/users/[userId]/[userName].tsx
@@ -1,5 +1,3 @@
-import MenuItem from '@/components/menu_item/MenuItem';
-
 //경로 https://stackoverflow.com/users/6117017/timbus-calin
 const UserDetail = () => {
   return <div>유저 상세보기 페이지입니다.</div>;
