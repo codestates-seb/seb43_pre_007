27d9c281bd1a08f1f5d66503e1a41061c09a689c
shdomi8599 <shdomi@naver.com>
2023-04-17 21:43:30 +0900

    feat : updateLocalStorge 함수

--- frontend/function/local_storage/setLocalStorage.ts
+++ frontend/function/local_storage/setLocalStorage.ts
@@ -3,11 +3,8 @@
  * @param key 저장할 새로운 키 값
  * @param data 저장할 값
  */
-export const setLocalStorage = (key: string, data: any) => {
+export const setLocalStorage = (key: string, data: string) => {
   if (typeof window !== 'undefined') {
-    localStorage.setItem(
-      key,
-      JSON.stringify({ date: new Date(), [`${key}`]: data })
-    );
+    localStorage.setItem(key, JSON.stringify([data]));
   }
 };
