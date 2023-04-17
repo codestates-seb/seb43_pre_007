c1f8ce6ebee608e2031d0218edf7a07f1a37d6a1
shdomi8599 <shdomi@naver.com>
2023-04-18 00:47:36 +0900

    refactor : Card 테스트코드 수정

--- frontend/components/card/Card.test.tsx
+++ frontend/components/card/Card.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Card from './Card';
+import styles from '@/styles/App.module.css';
 
 describe('Card 컴포넌트', () => {
   beforeEach(() => {
@@ -9,10 +10,20 @@ describe('Card 컴포넌트', () => {
 
   test('카드 스타일 체크', () => {
     const { getByText } = screen;
+    const borderGray = styles['--border-gray'];
     expect(getByText(/카드/)).toHaveStyle(`
-      border: 1px solid #cfd8df;
-      border-radius: 6px;
-      padding: 24px;
-    `);
+    border: 1px solid ${borderGray};
+    border-radius: 6px;
+    padding: 24px;
+  `);
   });
 });
+
+// test('카드 스타일 체크', () => {
+//   const { getByText } = screen;
+//   expect(getByText(/카드/)).toHaveStyle(`
+//   border: 1px solid var\(--border-gray\);
+//   border-radius: 6px;
+//   padding: 24px;
+// `);
+// });
