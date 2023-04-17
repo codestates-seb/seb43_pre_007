d856edd6c7719ce7e2ed1a8f1f5e3a3dc45c56bf
shdomi8599 <shdomi@naver.com>
2023-04-18 00:42:47 +0900

    refactor : 테스트 코드 수정

--- frontend/components/card/Card.test.tsx
+++ frontend/components/card/Card.test.tsx
@@ -1,27 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import Card from './Card';
+import styles from '@/styles/App.module.css';
 
 describe('Card 컴포넌트', () => {
   beforeEach(() => {
     const text = '카드';
-    render(
-      <Card>
-        <>{text}</>
-      </Card>
-    );
+    render(<Card>{text}</Card>);
   });
 
-  test('렌더링', () => {
-    const { getByText } = screen;
-    expect(getByText(/카드/)).toBeInTheDocument();
-  });
-
-  test('카드 스타일 체크', () => {
-    const { getByText } = screen;
-    expect(getByText(/카드/)).toHaveStyle(`
-      border: 1px solid #cfd8df;
-      border-radius: 6px;
-      padding: 24px;
-    `);
-  });
+test('카드 스타일 체크', () => {
+  const { getByText } = screen;
+  const borderGray = styles['--border-gray'];
+  expect(getByText(/카드/)).toHaveStyle(`
+    border: 1px solid ${borderGray};
+    border-radius: 6px;
+    padding: 24px;
+  `);
 });
+});
+
+// test('카드 스타일 체크', () => {
+//   const { getByText } = screen;
+//   const borderGray = styles['--border-gray'];
+//   expect(getByText(/카드/)).toHaveStyle(`
+//     border: 1px solid ${borderGray};
+//     border-radius: 6px;
+//     padding: 24px;
+//   `);
+// });
+
+// test('카드 스타일 체크', () => {
+//   const { getByText } = screen;
+//   expect(getByText(/카드/)).toHaveStyle(`
+//   border: 1px solid var\(--border-gray\);
+//   border-radius: 6px;
+//   padding: 24px;
+// `);
+// });
