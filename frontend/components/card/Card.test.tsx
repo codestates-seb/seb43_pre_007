45c4a2a1169ff44e68a38ca6ad39e4ef1aa26dab
shdomi8599 <shdomi@naver.com>
2023-04-18 00:58:39 +0900

    test : 충돌 해결 시도

--- frontend/components/card/Card.test.tsx
+++ frontend/components/card/Card.test.tsx
@@ -21,6 +21,16 @@ describe('Card 컴포넌트', () => {
 
 // test('카드 스타일 체크', () => {
 //   const { getByText } = screen;
+//   const borderGray = styles['--border-gray'];
+//   expect(getByText(/카드/)).toHaveStyle(`
+//     border: 1px solid ${borderGray};
+//     border-radius: 6px;
+//     padding: 24px;
+//   `);
+// });
+
+// test('카드 스타일 체크', () => {
+//   const { getByText } = screen;
 //   expect(getByText(/카드/)).toHaveStyle(`
 //   border: 1px solid var\(--border-gray\);
 //   border-radius: 6px;
