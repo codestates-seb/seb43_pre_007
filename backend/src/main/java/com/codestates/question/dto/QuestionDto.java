33e1060337488c600dd6b0a1edf1af0974ecb1b4
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2023-04-18 14:44:53 +0900

    feat : question êµ구현(미완)

--- backend/src/main/java/com/codestates/question/dto/QuestionDto.java
+++ backend/src/main/java/com/codestates/question/dto/QuestionDto.java
@@ -1,4 +1,76 @@
 package com.codestates.question.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+//TODO : DTO 작성
+//TODO : 유효성 검사
 public class QuestionDto {
+
+
+    @Getter
+    public static class Post{
+        //엥 유저아이디로 바꿔야함
+        private long userId;  // TODO : jwt로 유저정보를 받아와야한다... 일단 적용전까진 유저 정보를 수동으로 받고있음 (현재는 API 명세서대로 프론트에서 요청못받음)
+        private String title;
+        private String body;
+        private List<QuestionTagDto> tags;
+
+    }
+
+    @Getter
+    public static class Patch{
+        private long questionId;
+        private String title;
+        private String body;
+        private List<QuestionTagDto> tags;
+
+        public void setQuestionId(long questionId) {
+            this.questionId = questionId;
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class POSTResponse{
+        private QuestionUserResponseDto user;
+        private Response question;
+
+    }
+
+    @Getter
+    @Setter
+    public static class GETResponse{
+        private QuestionUserResponseDto user;
+        private Response question;
+        private List<QuestionAnswerResponseDto> answers;
+
+    }
+
+    @Getter
+    @Setter
+    public static class GETALLResponse {
+        private QuestionUserResponseDto user;
+        private Response question;
+
+    }
+
+    @Getter
+    @Setter
+    public static class Response{
+        private long questionId;
+        private String title;
+        private String body;
+        private boolean isAnswered;
+        private boolean isAccepted;
+        private int viewCount;
+        private int answerCount;
+        private LocalDateTime creationDate;
+        private LocalDateTime lastEditDate;
+        private List<QuestionTagDto> tags;
+
+    }
 }
