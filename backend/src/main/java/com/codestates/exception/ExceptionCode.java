38d808ddcc59b33837e6c98dd0b7cfbba878f52b
gtaegyeong <gangtaegyeong12@gmail.com>
2023-04-18 17:10:58 +0900

    feat: tag 에러코드 추가

--- backend/src/main/java/com/codestates/exception/ExceptionCode.java
+++ backend/src/main/java/com/codestates/exception/ExceptionCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ExceptionCode {
     USER_NOT_FOUND(404,"User not found"),
     USER_EXIST(409, "User exists"),
+    TAG_NOT_FOUND(404,"Tag not found"),
 
     //에러 코드 추가 - 이채은
     QUESTION_NOT_FOUND(404, "Question not found");
