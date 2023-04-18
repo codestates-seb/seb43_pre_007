b4047530a245de8d32dc0b421c7653093a51f5e9
shdomi8599 <shdomi@naver.com>
2023-04-18 23:47:32 +0900

    feat : users get api

--- frontend/__mocks__/api.ts
+++ frontend/__mocks__/api.ts
@@ -1,18 +1,16 @@
 import { rest } from 'msw';
-import { myListArr } from './dummy';
+import { users } from './dummy/users';
 
 export const handlers = [
   // rest.post('/sampleApi', async (req, res, ctx) => {
   //   const data = await req.json();
   //   return res(ctx.status(200), ctx.json({ result: data }));
   // }),
-  rest.post('/users/my_list', async (req, res, ctx) => {
-    const data = await req.json();
-    const filter = myListArr.find((x) => x.id === data);
-    if (!filter) {
-      return res(ctx.status(404));
-    }
-    const { myList } = filter;
-    return res(ctx.status(200), ctx.json(myList));
+  rest.get('/users', async (req, res, ctx) => {
+    const { size, page } = req.params;
+    const data = users.slice(Number(page) * 1, Number(size));
+    return res(ctx.status(200), ctx.json({ result: data }));
   }),
 ];
+
+// page 1 36 , page2 36 page 3 36
