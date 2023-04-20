97ddc9c0930fff8f9c59ba6ace1e503494bfc0ae
shdomi8599 <shdomi@naver.com>
2023-04-20 15:18:24 +0900

    refactor : dummy 변수명 변경

--- frontend/__mocks__/api.ts
+++ frontend/__mocks__/api.ts
@@ -1,13 +1,13 @@
 import { rest } from 'msw';
-import { Users } from './dummy/users';
+import { USERS } from './dummy/users';
 
 export const handlers = [
   rest.get('/users', async (req, res, ctx) => {
     const url = new URL(req.url);
     const size = Number(url.searchParams.get('size'));
     const page = Number(url.searchParams.get('page'));
-    const total = Users.length;
-    const data = Users.slice((page - 1) * size, page * size);
+    const total = USERS.length;
+    const data = USERS.slice((page - 1) * size, page * size);
     return res(ctx.status(200), ctx.json({ data, total }));
   }),
 ];
