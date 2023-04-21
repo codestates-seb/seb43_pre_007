27de9c42daba864712ac8768e9839c880060b728
shdomi8599 <shdomi@naver.com>
2023-04-21 12:16:36 +0900

    refactor : api

--- frontend/__mocks__/api.ts
+++ frontend/__mocks__/api.ts
@@ -4,9 +4,8 @@ import { questions } from './dummy/questions';
 
 export const handlers = [
   rest.get('/users', (req, res, ctx) => {
-    const url = new URL(req.url);
-    const size = Number(url.searchParams.get('size'));
-    const page = Number(url.searchParams.get('page'));
+    const size = Number(req.url.searchParams.get('size'));
+    const page = Number(req.url.searchParams.get('page'));
     const total = USERS.length;
     const data = USERS.slice((page - 1) * size, page * size);
     return res(ctx.status(200), ctx.json({ data, total }));
