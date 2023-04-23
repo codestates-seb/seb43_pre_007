073a1119b57ed93217b2a8056448b5c73de724b6
dev-Jaewon <app235naver@gmail.com>
2023-04-24 06:28:21 +0900

    feat - 질문하기 페이지

--- frontend/pages/questions/ask.tsx
+++ frontend/pages/questions/ask.tsx
@@ -1,7 +1,87 @@
-//경로 https://stackoverflow.com/questions/ask
+import styled from 'styled-components';
+import { QuestionAskForm } from '@/components/questionAskForm/QuestionAskForm';
 
 const Ask = () => {
-  return <div>질문하기 페이지입니다.</div>;
+  return (
+    <ScreenView>
+      <Container>
+        <AskHeader>
+          <h1>Ask a public question</h1>
+        </AskHeader>
+        <AskContent>
+          <QuestionAskForm />
+        </AskContent>
+      </Container>
+    </ScreenView>
+  );
 };
 
 export default Ask;
+
+const 도우미 = styled.div`
+  width: 315px;
+  background: blue;
+  margin-left: 24px;
+
+  @media (max-width: 770px) {
+    display: none;
+  }
+`;
+
+const ScreenView = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 50px 0 90px;
+  background-color: #f1f2f3;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: fit-content;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 1215px;
+  padding: 0 24px;
+  /* background: skyblue; */
+
+  @media (max-width: 640px) {
+    padding: 0px 16px;
+  }
+`;
+
+const AskHeader = styled.div`
+  display: flex;
+  justify-content: start;
+  width: 100%;
+
+  h1 {
+    display: flex;
+    align-items: center;
+    font-size: 26px;
+    font-weight: 500;
+    padding: 24px 0;
+    height: 130px;
+  }
+
+  @media (max-width: 640px) {
+    h1 {
+      font-size: 20px;
+      font-weight: 500;
+    }
+  }
+`;
+
+const AskContent = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: center;
+  width: 100%;
+
+  @media (max-width: 770px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
