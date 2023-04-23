d549bc65f3e77b2477aa1df6dbefc377738d9da2
dev-Jaewon <app235naver@gmail.com>
2023-04-23 20:35:18 +0900

    feat - react markdown 구현

--- frontend/components/markDownEditor/MarkDownEditor.tsx
+++ frontend/components/markDownEditor/MarkDownEditor.tsx
@@ -1,68 +1,129 @@
 import styled from 'styled-components';
-import { useRef } from 'react';
-import { Editor } from '@toast-ui/react-editor';
-import '@toast-ui/editor/dist/toastui-editor.css';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Editor } from 'codemirror';
 
-export type MarkDownEditorProps = {
-  content?: string;
-  onChangeText: (test: string | undefined) => void;
+import EasyMDE from 'easymde';
+import hljs from 'highlight.js';
+import 'easymde/dist/easymde.min.css';
+import 'highlight.js/styles/stackoverflow-light.css';
+import 'github-markdown-css/github-markdown.css';
+
+type MarkDownEditorProps = EasyMDE.Options & {
+  onChange: (v: string) => void;
+  preview?: boolean;
 };
 
-export const MarkDownEditor = (props: MarkDownEditorProps) => {
-  const editor = useRef<Editor>(null);
+const DEFAULT_OPTIONS: EasyMDE.Options = {
+  autoDownloadFontAwesome: true,
+  sideBySideFullscreen: false,
+  spellChecker: false,
+  renderingConfig: {
+    codeSyntaxHighlighting: true,
+    hljs,
+  },
+  status: false,
+  tabSize: 2,
+  lineWrapping: true,
+  maxHeight: '300px',
+  previewClass: ['markdown-body'],
+};
 
-  const handleEditorChange = () => {
-    const content = editor.current?.getInstance().getHTML();
-    props.onChangeText(content);
+export const MarkDownEditor = ({
+  preview,
+  onChange,
+  ...propsOptions
+}: MarkDownEditorProps) => {
+  const instance = useRef<EasyMDE | null>(null);
+  const codeMirror = useRef<CodeMirror.Editor | null>(null);
+  const [textArea, setTextArea] = useState<HTMLTextAreaElement | null>(null);
+
+  const handleChange = useCallback(
+    (ins: Editor) => onChange(ins.getValue()),
+    [onChange]
+  );
+
+  const setPreview = (ins: EasyMDE) => {
+    //@ts-ignore
+    ins.togglePreview();
   };
 
+  useEffect(() => {
+    if (!textArea) return;
+
+    if (preview) {
+      DEFAULT_OPTIONS['toolbar'] = false;
+      DEFAULT_OPTIONS['status'] = false;
+      DEFAULT_OPTIONS['maxHeight'] = 'auto';
+    }
+
+    const ins = new EasyMDE({
+      element: textArea,
+      ...DEFAULT_OPTIONS,
+      ...propsOptions,
+    });
+
+    instance.current = ins;
+    codeMirror.current = ins.codemirror;
+
+    if (preview) setPreview(ins);
+    //@ts-ignore
+    if (!preview) ins.toggleSideBySide();
+
+    return () => {
+      ins.toTextArea();
+      ins.cleanup();
+    };
+  }, [propsOptions, textArea, preview]);
+
+  useEffect(() => {
+    // setTextArea 되면 useEffect가 순서대로 실행이 되어, codeMirror.current.on이 실행이된다.
+    if (!codeMirror.current) return;
+
+    codeMirror.current.on('change', handleChange);
+
+    return () => {
+      codeMirror.current?.off('change', handleChange);
+    };
+  }, [textArea, handleChange]);
+
   return (
-    <Container>
-      <Editor
-        ref={editor}
-        height={'100%'}
-        previewStyle={'vertical'}
-        initialEditType={'markdown'}
-        initialValue={props.content}
-        onChange={handleEditorChange}
-      />
+    <Container preview={preview}>
+      <textarea ref={setTextArea} />
     </Container>
   );
 };
 
-const Container = styled.div`
-  border-radius: 4px;
-  height: 600px;
+const Container = styled.div<{ preview: boolean | undefined }>`
+  .EasyMDEContainer {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
 
-  @media (max-width: 640px) {
-    height: 800px;
-  }
+    .markdown-body {
+      margin-top: 10px;
+      width: 100% !important;
+      background-color: unset;
+      border: unset;
+      height: fit-content !important;
+      background-color: white;
 
-  &:focus-within {
-    outline: 1px solid rgba(0, 195, 255, 0.5);
-    box-shadow: 0 0 8px 2px rgba(4, 137, 247, 0.555);
-  }
+      p {
+        font-size: 1rem;
+      }
 
-  @media (max-width: 640px) {
-    .toastui-editor-md-container {
-      flex-direction: column;
+      pre {
+        max-height: 600px;
+      }
     }
 
-    .toastui-editor-main {
-      .toastui-editor {
-        width: 100%;
-      }
+    .EasyMDEContainer .CodeMirror {
+      border: 1px solid red !important;
+    }
 
-      .toastui-editor-md-splitter {
-        left: 0;
-        top: 50%;
-        width: 100%;
-        height: 1px;
-      }
-
-      .toastui-editor-md-preview {
-        width: 100%;
-      }
+    .CodeMirror {
+      width: 100% !important;
+      border: ${({ preview }) =>
+        preview ? 'none !important' : '1px solid #ced4da !important'};
     }
   }
 `;
