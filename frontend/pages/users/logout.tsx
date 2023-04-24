4970440ccfa0f260ab000a769536d8c18b3ddaaa
LimJooHun <jkjk8836@gmail.com>
2023-04-24 14:42:27 +0900

    Refactor: 필요 없는 부분 삭제

--- frontend/pages/users/logout.tsx
+++ frontend/pages/users/logout.tsx
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import Button from '@/components/button/Button';
+
 import faviconSprite from '@/public/faviconSprite.png';
 import { useRouter } from 'next/router';
 import { useRecoilState } from 'recoil';
@@ -53,7 +53,6 @@ const serviceList = [
 
 interface logout {
   e: React.FormEvent;
-  termId: string;
 }
 
 const logout = () => {
