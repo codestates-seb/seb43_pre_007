6c0312adc695b89d5780b2b6f3d57ef88812ae84
shdomi8599 <shdomi@naver.com>
2023-04-24 15:49:46 +0900

    feat : questions 페이지에 side_card 추가

--- frontend/components/side_card/SideCard.tsx
+++ frontend/components/side_card/SideCard.tsx
@@ -3,33 +3,51 @@ import styled from 'styled-components';
 
 type SideCardProps = {
   children: ReactNode;
+  textSize?: string;
 };
 
-const SideCard = (props: SideCardProps) => {
-  return <SideCardBox {...props} />;
+const SideCard = ({ textSize, ...props }: SideCardProps) => {
+  return <SideCardBox textSize={textSize} {...props} />;
 };
 
 export default SideCard;
 
-const SideCardBox = styled.div`
-  padding-bottom: 10px;
+const SideCardBox = styled.div<SideCardProps>`
   border: 1px solid var(--border-gray);
   border-radius: 6px;
+  box-shadow: var(--box-shadow);
+
   div {
     font-size: 0.78rem;
   }
 
   > div:first-child {
     padding: 15px 12px;
-    font-weight: 500;
+    font-weight: ${(props) => (props.textSize === 'sm' ? '100' : '500')};
     border-bottom: 1px solid var(--border-gray);
+    font-size: ${(props) => props.textSize === 'sm' && '0.95rem'};
   }
 
   > div:last-child {
-    margin: 12px 0px;
-    padding: 0px 12px;
-    div {
-      overflow-wrap: break-word;
+    overflow-wrap: break-word;
+
+    ul {
+      padding-bottom: 16px;
+
+      li {
+        padding: 14px 12px;
+        padding-bottom: 0px;
+        line-height: 15px;
+
+        > div:last-child {
+          cursor: pointer;
+        }
+
+        > a {
+          cursor: pointer;
+          color: var(--text-blue);
+        }
+      }
     }
   }
 `;
